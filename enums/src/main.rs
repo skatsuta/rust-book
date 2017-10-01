17819fc4410a7fa783a7f2facb91e5237a38f553
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-10-02 01:05:09 +0900

    add 6.1 Enums

---
+++ enums/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?} called", self)
+    }
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopbck = IpAddr::V6(String::from("::1"));
+    println!("home: {:?}, loopback: {:?}", home, loopbck);
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
