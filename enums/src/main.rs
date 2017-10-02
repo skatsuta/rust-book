3e56dd7815f5038339780092d6339eaf4c47450a
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-10-02 22:06:16 +0900

    add enum Coin exmaple

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -18,6 +18,23 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopbck = IpAddr::V6(String::from("::1"));
@@ -25,4 +42,7 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    let coin = Coin::Penny;
+    println!("{} cents", value_in_cents(coin));
 }
