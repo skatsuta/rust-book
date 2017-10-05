cd117e5d9e36c5f14f740d8a6c711a436bf6e31b
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-10-06 00:05:11 +0900

    add 11.1 adder

---
+++ adder/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_add_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "index out of bounds")]
+    fn index_out_of_bounds() {
+        vec![1][5];
+    }
+}
