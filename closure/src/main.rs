78995a07222fe6c0b8a8fc88da636e2f8e03b552
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-10-07 15:15:53 +0900

    add 13.1 Closures

---
+++ closure/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    {
+        let x = 4;
+        let equal_to_x = |z| z == x;
+        let y = 4;
+        assert!(equal_to_x(y));
+    }
+    {
+        let x = vec![1, 2, 3];
+        let equal_to_x = |z| z == x;
+        let y = vec![1, 2, 3];
+        assert!(equal_to_x(y));
+    }
+}
